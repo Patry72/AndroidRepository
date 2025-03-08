aefa3a202c7f2688866fd3ed29b76bc9c9ad9fb1
Patricia Cárdenas <cardenas.patri@gmail.com>
2025-03-08 13:41:02 +0100

    Añadido proyectos iniciales
    Proyectos de iniicio de sesion con google de prueba

---
+++ primer_login/lib/Pages/LoginPage.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+//import 'package:flutter_widgets/Pages/HomePage.dart';
+//import 'package:flutter_widgets/Services/auth_service.dart';
+
+import '../Services/auth_service.dart';
+import 'HomePage.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Center (
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                //Google Buttons
+                Padding(
+                  padding: const EdgeInsets.only(left: 4, right: 4, top: 2, bottom: 2),
+                  child: ElevatedButton(
+                    onPressed: () async {
+
+                      final credenciales = await AuthService().signInWithGoogle();
+
+                      debugPrint(credenciales.user?.displayName);
+                      debugPrint(credenciales.user?.photoURL);
+                      debugPrint(credenciales.user?.email);
+
+                      Navigator.push( context, MaterialPageRoute(builder: (context) => const HomePage()));
+
+                    },
+                    child: Text("Continua con Google"),
+                  ),
+                ),
+
+
+              ],
+            ),
+          )
+      ),
+    );
+  }
+}
